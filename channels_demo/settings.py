56bfc2ec4448bb483c99335cb5c3fde856eda45a
John Griebel <john@johngriebel.com>
2019-05-12 02:17:31 -0400

    Initial configuration of fantasy sports app + models
    Signed-off-by: John Griebel <john@johngriebel.com>

--- channels_demo/settings.py
+++ channels_demo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'fantasy_sports'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'channels_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'channels_demo',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
---
+++ fantasy_sports/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class ChannelsDemoBaseModel(models.Model):
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+    updated_timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Player(ChannelsDemoBaseModel):
+    player_id = models.IntegerField(unique=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    nba_team_id = models.IntegerField(null=True)
+    nba_team_abbreviation = models.CharField(max_length=5, blank=True, null=True)
+    age = models.DecimalField(max_digits=3, decimal_places=1)
+    gp = models.IntegerField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    min = models.DecimalField(max_digits=3, decimal_places=1)
+    fgm = models.DecimalField(max_digits=3, decimal_places=1)
+    fga = models.DecimalField(max_digits=3, decimal_places=1)
+    fg_pct = models.DecimalField(max_digits=4, decimal_places=3)
+    fg3m = models.DecimalField(max_digits=3, decimal_places=1)
+    fg3a = models.DecimalField(max_digits=3, decimal_places=1)
+    fg3_pct = models.DecimalField(max_digits=4, decimal_places=3)
+    ftm = models.DecimalField(max_digits=3, decimal_places=1)
+    fta = models.DecimalField(max_digits=3, decimal_places=1)
+    ft_pct = models.DecimalField(max_digits=4, decimal_places=3)
+    oreb = models.DecimalField(max_digits=3, decimal_places=1)
+    dreb = models.DecimalField(max_digits=3, decimal_places=1)
+    reb = models.DecimalField(max_digits=3, decimal_places=1)
+    ast = models.DecimalField(max_digits=3, decimal_places=1)
+    tov = models.DecimalField(max_digits=3, decimal_places=1)
+    stl = models.DecimalField(max_digits=3, decimal_places=1)
+    blk = models.DecimalField(max_digits=3, decimal_places=1)
+    blka = models.DecimalField(max_digits=3, decimal_places=1)
+    pf = models.DecimalField(max_digits=3, decimal_places=1)
+    pfd = models.DecimalField(max_digits=3, decimal_places=1)
+    pts = models.DecimalField(max_digits=3, decimal_places=1)
+    plus_minus = models.DecimalField(max_digits=3, decimal_places=1)
+
+    class Meta:
+        db_table = 'players'
+
+
+class FantasyLeague(ChannelsDemoBaseModel):
+    commissioner = models.ForeignKey(User, on_delete=models.CASCADE)
+    num_teams = models.IntegerField()
+    # Probably other stuff...
+
+    class Meta:
+        db_table = 'fantasy_league'
+
+
+class FantasyTeam(ChannelsDemoBaseModel):
+    fantasy_league = models.ForeignKey(FantasyLeague, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'fantasy_team'
+
+
+class PlayerFantasyTeamXRef(ChannelsDemoBaseModel):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    team = models.ForeignKey(FantasyTeam, null=True, on_delete=models.CASCADE)
+    league = models.ForeignKey(FantasyLeague, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'player_fantasy_team_xref'
