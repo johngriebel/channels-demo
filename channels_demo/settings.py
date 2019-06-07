5044686a9c09fb58b5ee5b7e71f694af7bd8c492
John Griebel <john@johngriebel.com>
2019-06-06 23:59:43 -0400

    Add support for CORS
    Signed-off-by: John Griebel <john@johngriebel.com>

--- channels_demo/settings.py
+++ channels_demo/settings.py
@@ -58,18 +58,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'channels',
     'django_extensions',
+    'corsheaders',
     'fantasy_sports'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'channels_demo.urls'
 
